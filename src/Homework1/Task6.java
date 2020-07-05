757b6780757b52781f5ed246b3ea5ab1a2a158c2
Вадим <vadim.malynich@mail.ru>
2020-07-05 20:40:27 +0300

    Homework

---
+++ src/Homework1/Task6.java
@@ -0,0 +1,35 @@
+package Homework1;
+
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        String str1, str2;
+        int count=0;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Введите первую строку: ");
+        str1=scanner.nextLine();
+        System.out.print("Введите вторую строку: ");
+        str2=scanner.nextLine();
+        char string1[]=str1.toCharArray();
+        char string2[]=str2.toCharArray();
+        char temp[];
+        for(int i=0;i<string1.length;i++){
+            for(int j=0;j<string2.length;j++){
+                if(string1[i]==string2[j]){
+                    count++;
+                    for(int k=j;k<string2.length-1;k++){
+                        string2[k]=string2[k+1];
+                    }
+                    break;
+                }
+            }
+        }
+        if(count==string1.length){
+            System.out.println("Одна строка является перестановкой другой!");
+        }else{
+            System.out.println("Одна строка не является перестановкой другой!");
+        }
+        scanner.close();
+    }
+}
