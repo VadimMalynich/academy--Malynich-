6f34e13911663826a20f209ba23374298d5d849e
Вадим <vadim.malynich@mail.ru>
2020-08-25 18:06:53 +0300

    Homework

--- src/by/academy/deal2/Main.java
+++ src/by/academy/deal2/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         ArrayList<Product> product = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int flag;
